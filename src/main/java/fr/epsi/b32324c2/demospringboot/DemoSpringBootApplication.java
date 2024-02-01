15d51a8ba2130fb7fb2286ccaa246d7e82a64ef5
Quentin Verdier <quent1.verdier@gmail.com>
2024-02-01 10:37:33 +0100

    Initial commit

---
+++ src/main/java/fr/epsi/b32324c2/demospringboot/DemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.b32324c2.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/epsi/b32324c2/demospringboot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.b32324c2.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
